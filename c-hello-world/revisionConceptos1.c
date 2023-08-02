39bdf59a5916e6822b8888bccd1c6c67abbd7054
Joaquín <joaquingomezleites@gmail.com>
2023-08-02 00:49:19 -0300

    Initial commit

---
+++ c-hello-world/revisionConceptos1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>   // libreria
+#include <string.h>  // libreria
+
+// %d --> int
+// %c --> char
+// %f --> float
+// %s --> strings
+
+struct fecha {
+    int dia;
+    char mes[12];
+    int anio;
+    char bisiesto;
+};
+
+main() {  // falta int main porque hay un return 0; al final
+    struct fecha f1, f2;
+
+    f1.dia = 30;
+    // f1.mes[0] = 'enero'; // no son las comillas correctas y no se puede
+    // reasignar la memoria
+    f1.dia = 48;
+    f1.anio = 2000;
+    // f1.bisiesto = si; // faltan comillas y bisiesto es un char
+
+    f2.anio = f1.dia;
+    f2.bisiesto = 'n';
+    printf("%d", f1.mes);
+
+    if (f2.anio < f1.anio) printf("%c", f2.mes);
+
+    return 0;
+};
---
+++ c-hello-world/revisionConceptos2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// estructura 1
+struct estr1 {
+    int campo1;
+    char campo2, campo3;
+    int campo4[10];
+};
+
+// estructura 2
+struct estr2 {
+    struct estr1 camp1;
+    int camp2;
+};
+
+void main() {
+    struct estr2 e, arrE[20];
+    struct estr1 e1;
+};
+
+// decir el tipo de las expresiones que se dan a continuación
+// i. arrE[5] ------> ARREGLO de tipo estr2
+// ii. e[0].camp1 ------>  estructura de tipo estr1
+// iii. arrE[1].camp1.campo1 ------> int
+// iv. e[5].camp2 ------> int
+// v. arrE[0].camp1.campo4 ------> int
+// vi. arrE[0].camp1.campo4[8] ------>int en la posición 8
+// vii. e1.campo4 ------> array de int
+// viii. e1.campo3 ------> char
