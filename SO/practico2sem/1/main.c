b240c05147af2cefb9f7cbe7488ac29a4454a51c
Joaquín <joaquingomezleites@gmail.com>
2023-09-21 00:20:54 -0300

    todo

--- SO/practico2sem/1/main.c
+++ SO/practico2sem/1/main.c
@@ -27,19 +27,29 @@
 // gcc -o main main.c -lpthread -lrt
 
 // cocinero
-void coloca_plato() {
-    // algo con la shm
+void coloca_plato()
+{
+    printf("cocinero coloca plato pid: %d | ppid: %d \n", getpid(), getppid());
+    sleep(1);
+    printf("cocinero termina de colocar plato pid: %d | ppid: %d \n", getpid(), getppid());
 }
 
 // mozo
-void levanta_pedido() {
-    // algo con la shm
+void levanta_pedido()
+{
+    printf("mozo levanta pedido pid: %d | ppid: %d \n", getpid(), getppid());
+    sleep(1);
+    printf("mozo terminó levanta pedido pid: %d | ppid: %d \n", getpid(), getppid());
 }
-void entrega_pedido() {
-    // algo con la shm
+void entrega_pedido()
+{
+    printf("mozo entrega pedido pid: %d | ppid: %d \n", getpid(), getppid());
+    sleep(1);
+    printf("mozo terminó entrega pedido pid: %d | ppid: %d \n", getpid(), getppid());
 }
 
-int main() {
+int main()
+{
     // ---------- Shared memory (mostrador) ----------
     int shm_fd;
     int *mostrador = 0;
@@ -50,57 +60,70 @@ int main() {
     ftruncate(shm_fd, 4096);
 
     mostrador = (int *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    *mostrador = 12;
+    *mostrador = 0;
     // -----------------------------------------------
 
     // ---------- Semaphore sc & m & c ----------
     sem_t *sem_sc, *sem_m, *sem_c;
-    sem_sc = sem_open("sem", O_CREAT, 0644, 12);     // sem shm inicilizado en 1
-    sem_m = sem_open("mozo", O_CREAT, 0644, 1);      // sem mozo inicilizado en 1
-    sem_c = sem_open("cocinero", O_CREAT, 0644, 1);  // sem cocinero inicilizado en 1
+    sem_sc = sem_open("sem", O_CREAT, 0644, 1);      // sem shm inicilizado en 1
+    sem_m = sem_open("mozo", O_CREAT, 0644, 0);      // sem mozo inicilizado en 1
+    sem_c = sem_open("cocinero", O_CREAT, 0644, 12); // sem cocinero inicilizado en 12
     // -------------------------------------
 
     // 2 cocineros y 3 mozos, creamos 5 procesos
-    int valueSem;
-    for (int i = 0; i < 3; i++)
-    // creamos 3 mozos
-    {
-        int p = fork();
-
-        if (p == 0) {  // estamos en el hijo (mozo)
-            sem_getvalue(&sem_sc, &valueSem);
-            if (valueSem < 12) {
-                // hay platos en el mostrador
-                sem_wait(sem_sc);
-                levanta_pedido();
-                entrega_pedido();
-                sem_post(sem_sc);
-            }
-            printf("mozo pid: %d | ppid: %d \n", getpid(), getppid());
-            exit(-1);
-        }
-        if (p > 0) {
-            // padre (no hacer nada)
-        }
-    }
 
     for (int i = 0; i < 2; i++)
     // creamos 2 cocineros
     {
         int q = fork();
-
-        if (q == 0) {  // estamos en el hijo (cocinero)
-            sem_getvalue(&sem_sc, &valueSem);
-            if (valueSem > 0) {
-                // hay espacios libres en el mostrador
-                sem_wait(sem_sc);
-                coloca_plato();
-                sem_post(sem_sc);
-            }
+        if (q == 0)
+        { // estamos en el hijo (cocinero)
             printf("cocinero pid: %d | ppid: %d \n", getpid(), getppid());
+            while (1)
+            {
+                sem_wait(sem_c); // -1
+                printf("%d", *mostrador);
+                coloca_plato();
+                // mostrador memoria compartida
+                sem_wait(sem_sc); // -1
+                *mostrador++;
+                sem_post(sem_sc); // +1
+
+                sem_post(sem_m); // +1
+            }
             exit(-1);
         }
-        if (q > 0) {
+        if (q > 0)
+        {
+            // padre (no hacer nada)
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    // creamos 3 mozos
+    {
+        int p = fork();
+        if (p == 0)
+        { // estamos en el hijo (mozo)
+            printf("mozo pid: %d | ppid: %d \n", getpid(), getppid());
+            while (1)
+            {
+                // Hay platos en el mostrador
+                sem_wait(sem_m); // -1
+                printf("%d", *mostrador);
+                levanta_pedido();
+                // mostrador memoria compartida
+                sem_wait(sem_sc); // -1
+                *mostrador--;
+                sem_post(sem_sc); // +1
+                entrega_pedido();
+
+                sem_post(sem_c); // +1
+            }
+            exit(-1);
+        }
+        if (p > 0)
+        {
             // padre (no hacer nada)
         }
     }
---
+++ SO/practico2sem/2/main.c
@@ -0,0 +1,56 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+// Modelar un juego de cartas donde participan 10 jugadores. Los jugadores
+// deberán primero pensar la jugada y luego jugar, pero solo podrán hacerlo
+// cuando les toque su turno (por orden de ubicación en la ronda).
+// Se dispone del tipo carta y los siguientes procedimientos y funciones:
+// typedef carta{
+// int num; // valor numerico
+// char palo; // o=oro, e=espada, b=basto, c=copa
+// • carta pensar() : que ejecutada por el jugador piensa cual será la
+// próxima jugada y retorna una carta (elegida aleatoriamente).
+// Se debe implementar el tipo jugador y el árbitro usando semáforos. La
+// jugada se muestra por pantalla.
+
+// gcc -o main main.c -lpthread -lrt
+
+typedef struct carta {
+    int num;    // valor numerico
+    char palo;  // o=oro, e=espada, b=basto, c=copa
+};
+
+void pensar() {
+    srand(time(NULL));
+    int jugada = rand() % 10;
+    printf("pienso en %d \n", jugada);
+};
+
+int main() {
+    srand(time(NULL));
+
+    for (int i = 0; i < 5; i++)
+    // creamos 10 jugadores
+    {
+        int p = fork();
+        if (p == 0) {  // estamos en el hijo (jugador)
+            printf("mozo pid: %d | ppid: %d \n", getpid(), getppid());
+            int jugada = rand() % 10;
+            printf("pienso en %d \n", jugada);
+            exit(-1);
+        }
+        if (p > 0) {
+            // padre (no hacer nada)
+        }
+    }
+    return 0;
+}
